4372b0a4cabc366b9bb2e2d4f573d49a72a09583
Logon2390 <jorgeluis7040@gmail.com>
2026-04-17 20:32:04 -0600

    Refactor SpriteBuilder to support multiple arrow sprites and update createArrow method signature

--- src/builders/SpriteBuilder.cpp
+++ src/builders/SpriteBuilder.cpp
@@ -1,9 +1,23 @@
 #include "SpriteBuilder.hpp"
 
-CCSprite *SpriteBuilder::createArrow(bool flipped, float scale) {
-  auto spr = CCSprite::createWithSpriteFrameName(arrowSprName);
-  spr->setScale(scale);
-  spr->setFlipX(flipped);
-  return spr;
+CCSprite *SpriteBuilder::createArrow(ArrowSprite sprite, bool flipped, float scale)
+{
+    auto spr = CCSprite::createWithSpriteFrameName(formatArrowSpriteName(sprite));
+    spr->setScale(scale);
+    spr->setFlipX(flipped);
+    return spr;
+}
+
+const char* SpriteBuilder::formatArrowSpriteName(ArrowSprite sprite) {
+    switch (sprite) {
+        case ArrowSprite::Green:
+            return SpriteBuilder::arrow1SprName;
+        case ArrowSprite::Cyan:
+            return SpriteBuilder::arrow2SprName;
+        case ArrowSprite::Pink:
+            return SpriteBuilder::arrow3SprName;
+        default:
+            return SpriteBuilder::arrow1SprName;
+    }
 }
 
--- src/builders/SpriteBuilder.hpp
+++ src/builders/SpriteBuilder.hpp
@@ -2,12 +2,34 @@
 
 using namespace geode::prelude;
 
+enum class ArrowSprite {
+    Green,
+    Cyan,
+    Pink,
+};
+
 namespace SpriteBuilder {
     constexpr const char *bigFontName = "bigFont.fnt";
     constexpr const char *goldFontName = "goldFont.fnt";
-    constexpr const char* arrowSprName = "GJ_arrow_01_001.png";
+    constexpr const char* arrow1SprName = "GJ_arrow_01_001.png";
+    constexpr const char* arrow2SprName = "GJ_arrow_02_001.png";
+    constexpr const char* arrow3SprName = "GJ_arrow_03_001.png";
     constexpr const char *infoIconName = "GJ_infoIcon_001.png";
-    constexpr const char *backgroundSpriteName = "square02b_001.png";
+    constexpr const char *backgroundSprName = "square02b_001.png";
+    constexpr const char *squareSprName = "square.png";
     constexpr const char *hideSprName = "hideBtn_001.png";
-    CCSprite *createArrow(bool flipped = false, float scale = 0.6f);
+    constexpr const char *helpBtnSprName = "GJ_helpBtn_001.png";
+    constexpr const char *optionsBtnSprName = "GJ_optionsBtn_001.png";
+    constexpr const char *stopEditorBtnSprName = "GJ_stopEditorBtn_001.png";
+    constexpr const char *playEditorBtnSprName = "GJ_playEditorBtn_001.png";
+    constexpr const char *folderBtnSprName = "gj_folderBtn_001.png";
+    constexpr const char *lockOpenSprName = "GJ_lock_open_001.png";
+    constexpr const char *lockClosedSprName = "GJ_lock_001.png";
+    constexpr const char *resetBtnSprName = "GJ_resetBtn_001.png";
+    constexpr const char *heartOffSprName = "gj_heartOff_001.png";
+    constexpr const char *heartOnSprName = "gj_heartOn_001.png";
+    constexpr const char *inUseBtnSprName = "GJ_selectSongOnBtn_001.png";
+    constexpr const char *useBtnSprName = "GJ_selectSongBtn_001.png";
+    const char* formatArrowSpriteName(ArrowSprite sprite);
+    CCSprite *createArrow(ArrowSprite sprite, bool flipped = false, float scale = 0.6f);
 }
